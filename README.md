c1203fdb4070279a5a7e755b09f1c059793c9370
dazcona <david.azcona@asu.edu>
2019-11-16 14:57:11 +0000

    drone takes off

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # droneloco
+
+Build image:
+docker-compose up --build
+
+Run image:
+docker-compose up
+docker-compose up -d
+
+docker exec -it drone bash
--- src/app.py
+++ src/app.py
@@ -1,11 +1,46 @@
 from flask import Flask
 from flask import render_template
-  
+import tello
+import time
+from PIL import Image
+
+
 app = Flask(__name__) 
   
 @app.route('/') 
 def index(): 
     return render_template('index.html')
+
+
+@app.route('/video') 
+def video():
+
+    drone = tello.Tello('', 8889) 
+
+    drone.takeoff()
+
+    time.sleep(5)
+
+    while True:
+
+        print('Reading drone!')
+        frame = drone.read()
+
+        if frame is None or frame.size == 0:
+            print('Nothing!')
+            continue
+
+        print('Get Image!')
+        image = Image.fromarray(frame)
+
+        print('Save Image!')
+        image.save("drone.jpg")
+
+        time.sleep(3)
+
+
+    return render_template('video.html')
+
   
 if __name__ == '__main__': 
   
