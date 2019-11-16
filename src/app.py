5ba7f70ac2e23691b4d070f8673d1edf28fb9f71
dazcona <david.azcona@asu.edu>
2019-11-16 17:20:33 +0000

    video not working

--- src/app.py
+++ src/app.py
@@ -3,6 +3,8 @@ from flask import render_template
 import tello
 import time
 from PIL import Image
+import threading
+from control import TelloUI
 
 
 app = Flask(__name__) 
@@ -11,33 +13,36 @@ app = Flask(__name__)
 def index(): 
     return render_template('index.html')
 
-
 @app.route('/video') 
 def video():
 
-    drone = tello.Tello('', 8889) 
+    drone = tello.Tello('', 8889)
+    vplayer = TelloUI(drone, "/code/pictures/")
+    time.sleep(2)
+    print('Stopping!')
+    vplayer.onClose()
 
-    drone.takeoff()
+    # try:
+    #     result = drone.takeoff()
+    #     time.sleep(2)
+    #     result = drone.flip('l')
+    #     time.sleep(2)
+    # except:
+    #     print('Error')
+    # finally:
+    #     drone.land()
 
-    time.sleep(5)
-
-    while True:
-
-        print('Reading drone!')
-        frame = drone.read()
-
-        if frame is None or frame.size == 0:
-            print('Nothing!')
-            continue
-
-        print('Get Image!')
-        image = Image.fromarray(frame)
-
-        print('Save Image!')
-        image.save("drone.jpg")
-
-        time.sleep(3)
-
+    # while True:
+    #     print('Reading drone!')
+    #     frame = drone.read()
+    #     if frame is None or frame.size == 0:
+    #         print('Nothing!')
+    #         continue
+    #     print('Get Image!')
+    #     image = Image.fromarray(frame)
+    #     print('Save Image!')
+    #     image.save("drone.jpg")
+    #     time.sleep(3)
 
     return render_template('video.html')
 
