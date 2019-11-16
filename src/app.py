f7a8aeac209d34553217a743fada2fecd6690a04
Enric Moreu <enric.moreu.filella@gmail.com>
2019-11-16 15:27:07 +0000

    Basic buttons connected to backend

--- src/app.py
+++ src/app.py
@@ -6,7 +6,27 @@ app = Flask(__name__)
 @app.route('/') 
 def index(): 
     return render_template('index.html')
-  
+
+@app.route('/forward') 
+def forward(): 
+    print('forward!!')
+    return "200"
+
+@app.route('/right') 
+def right(): 
+    print('right!!')
+    return "200"
+
+@app.route('/back') 
+def back(): 
+    print('back!!')
+    return "200"
+
+@app.route('/left') 
+def left(): 
+    print('left!!')
+    return "200"
+
 if __name__ == '__main__': 
   
     app.run(debug=True, host='0.0.0.0') 
