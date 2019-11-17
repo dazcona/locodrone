ded72179f9e0764d346d572239bb2f29d2b0dbec
Juan José Nieto <juanjo.3ns@gmail.com>
2019-11-17 11:13:57 +0100

    BGR to RGB

--- src/VideoDrone.py
+++ src/VideoDrone.py
@@ -10,8 +10,10 @@ class VideoDrone(object):
 
 	def get_frame(self):
 		frame = self.cap.read()
+
 		if frame is None or frame.size == 0:
 			return ''
+		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 		print(frame.shape)
 		(flag, encodedImage) = cv2.imencode(".jpg", frame)
 		return encodedImage.tobytes()
